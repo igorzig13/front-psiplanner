456f31c7cfabd7d922b525731d4feff044c80328
João Pedro Fontes <joaopedrofonttes@gmail.com>
2024-11-23 17:03:45 -0300

    create client-homepage

--- src/app/pages/client-homepage/client-homepage.component.ts
+++ src/app/pages/client-homepage/client-homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HorizontalCardComponent } from '../../components/horizontal-card/horizontal-card.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-client-homepage',
   standalone: true,
-  imports: [ HorizontalCardComponent ],
+  imports: [ HorizontalCardComponent , ButtonComponent],
   templateUrl: './client-homepage.component.html',
   styleUrl: './client-homepage.component.css'
 })
