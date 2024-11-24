4bca4491b4d503646dd7a355f91255edf14466be
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2024-11-24 14:16:37 -0300

    Add dados da consulta
    Adição do titulo da consulta

--- src/app/pages/client-calendario-consulta-agendada/client-calendario-consulta-agendada.component.ts
+++ src/app/pages/client-calendario-consulta-agendada/client-calendario-consulta-agendada.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterViewInit,ElementRef } from '@angular/core';
+import { Component} from '@angular/core';
 import { DefaultCardComponent } from "../../components/default-card/default-card.component";
 import { PopupComponent } from '../../components/popup/popup.component';
 import { ButtonComponent } from "../../components/button/button.component";
@@ -12,19 +12,9 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
   styleUrl: './client-calendario-consulta-agendada.component.css'
 })
 export class ClientCalendarioConsultaAgendadaComponent {
-  @ViewChild('popup',{read:ElementRef}) popupElement!:ElementRef;
   navUi:number = 2;
   openAgendamento: boolean = false;
   rating:number=4;
-
-  ngAfterViewInit() {
-    const popupHeader = this.popupElement.nativeElement.querySelector('.popup-header');
-    if (this.openAgendamento) {
-      const customTitle = document.createElement('h1');
-      customTitle.textContent = 'Este é o título fora do popup-header';
-      popupHeader.parentElement.insertBefore(customTitle, popupHeader);
-    }
-  }
 
   toggleAgendamento(){
     this.openAgendamento = !this.openAgendamento;
