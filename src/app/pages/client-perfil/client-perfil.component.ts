072e771fcda8f3694c22d783a40a7129c34d57fc
Vitor Emanuel <vitoralencar2706@gmail.com>
2024-11-26 17:44:47 -0300

    refactor forms #2

--- src/app/pages/client-perfil/client-perfil.component.ts
+++ src/app/pages/client-perfil/client-perfil.component.ts
@@ -2,16 +2,20 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ButtonComponent } from "../../components/button/button.component";
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validator, FormControlName } from '@angular/forms';
+import { MessageComponent } from '../../components/message/message.component';
 
 @Component({
   selector: 'app-client-perfil',
   standalone: true,
-  imports: [NavbarComponent, ButtonComponent, ReactiveFormsModule],
+  imports: [NavbarComponent, ButtonComponent, ReactiveFormsModule, MessageComponent],
   templateUrl: './client-perfil.component.html',
   styleUrl: './client-perfil.component.css'
 })
 export class ClientPerfilComponent {
   navUi: number = 2;
+
+  openMessage: boolean = false;
+  message: string = '';
 
   formUpdate: FormGroup = new FormGroup({});
 
@@ -25,17 +29,27 @@ export class ClientPerfilComponent {
     this.formUpdate = this.formBuilder.group({
       email: ['', []],
       telephone: ['', []],
-      location: ['', []],
-      password: ['', []],
+      password: ['', []]
     });
   }
 
   send() {
     console.log(this.formUpdate.value);
     this.formUpdate.reset();
+    this.toggleMessage('Seus dados foram atualizados!');
   }
 
   cancel() {
     this.formUpdate.reset();
+  }
+
+  toggleMessage(message: string) {
+    this.message = message;
+    this.openMessage = !this.openMessage;
+
+    setTimeout(() => {
+      this.openMessage = !this.openMessage;
+      this.message = '';
+    }, 5000);
   }
 }
