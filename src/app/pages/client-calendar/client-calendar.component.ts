276c82e2f9b46a6b05215e45c9b350ece847f780
Vitor Emanuel <vitoralencar2706@gmail.com>
2024-11-26 21:05:52 -0300

    fix bugs #1

--- src/app/pages/client-calendar/client-calendar.component.ts
+++ src/app/pages/client-calendar/client-calendar.component.ts
@@ -4,16 +4,26 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { CalendarComponent, CalendarDate } from '../../components/calendar/calendar.component';
 import { PopupComponent } from '../../components/popup/popup.component';
 import { DefaultCardComponent } from '../../components/default-card/default-card.component';
+import { FormsModule } from '@angular/forms';
+import { ConfirmationFormComponent } from "../../components/confirmation-form/confirmation-form.component";
+import { MessageComponent } from '../../components/message/message.component';
 
 @Component({
   selector: 'app-client-calendar',
   standalone: true,
-  imports: [ButtonComponent, NavbarComponent, CalendarComponent, PopupComponent, DefaultCardComponent],
+  imports: [ButtonComponent, NavbarComponent, CalendarComponent, PopupComponent,
+            DefaultCardComponent, FormsModule, ConfirmationFormComponent, MessageComponent],
   templateUrl: './client-calendar.component.html',
   styleUrl: './client-calendar.component.css'
 })
 export class ClientCalendarComponent {
   navUi: number = 2;
+
+  rating: string = '';
+  comment: string = '';
+  message: string = '';
+
+  error: boolean = false;
 
   selectedPacient: any = null;
   dateString: string = '';
@@ -21,6 +31,8 @@ export class ClientCalendarComponent {
   openNextConsults: boolean = false;
   openDoneConsults: boolean = false;
   openAvailable: boolean = false;
+  openConfirmation: boolean = false;
+  showMessage: boolean = false;
 
   previousProfessional = { name: 'Aldo Queiroz', url_img: 'https://placehold.co/450', rating: 5 };
   nextProfessional = { name: 'João Pedro', url_img: 'https://placehold.co/300', rating: 4.9 };
@@ -31,6 +43,48 @@ export class ClientCalendarComponent {
 
   toggleDoneConsults() {
     this.openDoneConsults = !this.openDoneConsults;
+  }
+
+  toggleAvailable(professional: any) {
+    this.openAvailable = !this.openAvailable;
+  }
+
+  toggleConfirmation() {
+    if (this.openConfirmation) {
+      /* Empty. */
+      this.openConfirmation = !this.openConfirmation;
+      this.error = false;
+      return;
+    }
+
+    if (!this.openConfirmation) {
+      if (this.rating !== '') {
+        this.openConfirmation = !this.openConfirmation;
+        return;
+      }
+      this.error = true;
+    }
+  }
+
+  toggleMessage() {
+    this.showMessage = !this.showMessage;
+
+    setTimeout(() => {
+      this.showMessage = !this.showMessage;
+      this.message = '';
+    }, 5000);
+  }
+
+  sendAvailable(sucess: boolean) {
+    if (sucess) {
+      console.log('Classificação: ' + this.rating + ' - ' + this.comment);
+      this.comment = '';
+      this.rating = '';
+      this.toggleConfirmation();
+      this.toggleAvailable(this.previousProfessional);
+      this.message = 'Avaliação enviada com sucesso!';
+      this.toggleMessage();
+    }
   }
 
   calendarEventListener(day: CalendarDate) {
